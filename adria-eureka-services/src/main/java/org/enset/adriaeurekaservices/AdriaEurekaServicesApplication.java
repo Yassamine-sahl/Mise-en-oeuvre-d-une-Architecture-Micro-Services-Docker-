029a09eb59bd455a33b16e61e719a62096c02cbc
Yassamine-sahl <85345273+Yassamine-sahl@users.noreply.github.com>
2022-10-24 22:17:56 +0100

    Bonnes Pratiques : Eureka Discovery

---
+++ adria-eureka-services/src/main/java/org/enset/adriaeurekaservices/AdriaEurekaServicesApplication.java
@@ -0,0 +1,15 @@
+package org.enset.adriaeurekaservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AdriaEurekaServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdriaEurekaServicesApplication.class, args);
+    }
+
+}
